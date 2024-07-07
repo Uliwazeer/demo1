2955cf35ff73e87bb366c7cf667582503928c874
Ali Wazeer <84068430+Uliwazeer@users.noreply.github.com>
2024-07-07 15:10:30 +0300

    Update PersonService.java
    Adding New Person Details For New Person Feature

--- src/main/java/com/example/service/PersonService.java
+++ src/main/java/com/example/service/PersonService.java
@@ -39,6 +39,12 @@ public class PersonService {
 		p.setFirstName("AbdulHalim");
 		p.setLastName("Mahmoud");
 		persons.add(p);
+		p = new Person();
+		p.setId("77");
+		p.setAge(25);
+		p.setFirstName("AbdulHalim2");
+		p.setLastName("Mahmoud2");
+		persons.add(p);
 		
 	}
 	public Person getPerson(String id) {
